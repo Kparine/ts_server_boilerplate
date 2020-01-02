5c44fc36c640f506b31e99c019b426909729c19d
Kevin Parine <kparine314@gmail.com>
2020-01-01 23:09:03 -0800

    Get Express Server Running

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ts_server_boilerplate
+
+#### To Compile:
+
+* Run`tsc`
+
+#### To Run:
+
+* `node dist/index.ts`
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app: express.Express = express();
 const port: number = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => { console.log(`****---->>>> Listening on Port: ${port} <<<<----****`); });
