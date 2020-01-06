9bcf37d6cae4ed5424d5637f0b075609608e70e7
Kevin Parine <kparine314@gmail.com>
2020-01-05 19:37:24 -0800

    remove unused auth code

--- src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-
-// const router = express.Router();
-// import authController from "../controllers/auth";
-
-// router.get("/", authController.authenticated, authController.status, authController.confirmed);
-// router.post("/", authController.login);
-
-// module.exports = router;
