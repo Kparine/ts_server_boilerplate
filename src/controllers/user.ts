f0d5d2150f0490177c2a74d906fc74063a66644c
Kevin Parine <kparine314@gmail.com>
2020-01-18 19:44:50 -0800

    Resolve bcrypt hashing issues and audit pakages

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,6 +2,18 @@ import * as express from "express";
 import * as userModel from "../models/user";
 import { IUser } from "../types";
 
+export const getUsers: express.RequestHandler = (req: express.Request, res: express.Response): void => {
+    try {
+        const users: IUser[] = userModel.getUsers();
+        if (!users) {
+            res.status(404);
+        }
+        res.status(200).send(users);
+    } catch (err) {
+        res.status(500).send();
+    }
+};
+
 export const getUser: express.RequestHandler = (req: express.Request, res: express.Response): void => {
     try {
         const user: IUser | null = userModel.getUser(req.params.id);
@@ -22,6 +34,19 @@ export const createUser: express.RequestHandler = async (req: express.Request, r
             res.status(404).send({ status: 400, message: "Unable to create user" });
         }
         res.status(200).send(userId);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+};
+
+export const updateUser: express.RequestHandler = (req: express.Request, res: express.Response): void => {
+    try {
+        const user: IUser | null = userModel.updateUser(req.body);
+        if (!user) {
+            res.status(404).send({ status: 400, message: "Unable to update user" });
+        } else {
+            res.status(200).send(user);
+        }
     } catch (err) {
         res.status(500).send(err);
     }
